55f2c020c66b29fdf5d43a62b080b20091643850
Rik Meijer <hello@rikmeijer.nl>
2024-09-10 21:29:14 +0200

    refactor

--- src/Filters.php
+++ src/Filters.php
@@ -40,11 +40,18 @@ class Filters {
         };
     }
     
+    static function buildScalarTest(string $event_field) {
+        return fn(array $filter_values, array $event) => in_array($event[$event_field], $filter_values);
+    }
+    static function buildTagTest(string $event_tag_identifier) {
+        return fn(array $filter_values, array $event) => some($event['tags'], fn(array $event_tag) => $event_tag[0] === $event_tag_identifier && in_array($event_tag[1], $filter_values));
+    }
+    
     static function scalar(string $filter_field, string $event_field) : callable {
-        return self::buildFilter($filter_field, fn(array $filter_values, array $event) => in_array($event[$event_field], $filter_values));
+        return self::buildFilter($filter_field, self::buildScalarTest($event_field));
     }
     
     static function tag(string $filter_tag_identifier, string $event_tag_identifier) : callable {
-        return self::buildFilter($filter_tag_identifier, fn(array $filter_values, array $event) => some($event['tags'], fn(array $event_tag) => $event_tag[0] === $event_tag_identifier && in_array($event_tag[1], $filter_values)));
+        return self::buildFilter($filter_tag_identifier, self::buildTagTest($event_tag_identifier));
     }
 }

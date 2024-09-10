230aa322e836a4423dd584d532192b35fe27f5be
Rik Meijer <hello@rikmeijer.nl>
2024-09-10 15:23:26 +0200

    close relay when shutting down agent

--- agent.php
+++ agent.php
@@ -1,13 +1,33 @@
 <?php
+pcntl_async_signals(TRUE);
 
 require_once __DIR__ . '/bootstrap.php';
 
-\Transpher\Nostr\Server::boot($_SERVER['argv'][1], [], function(callable $relay) {
-    $relay_url = 'ws://127.0.0.1:' . $_SERVER['argv'][1];
-    $agent = new \Transpher\WebSocket\Client(new WebSocket\Client($relay_url));
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 
-    echo PHP_EOL . "Listening to server @ " . $relay_url .  "...";
-    echo PHP_EOL;
+
+
+
+\Transpher\Nostr\Server::boot($_SERVER['argv'][1], [], function(callable $relay) {
+    \Functional\each(\Functional\filter(get_defined_constants(), fn(mixed $value, string $name) => str_starts_with($name, 'SIG')), function(mixed $value, string $name) use ($relay) {
+        if (str_starts_with($name, 'SIG_')) {
+        } elseif ($value === SIGKILL) {
+        } elseif ($value === SIGSTOP) {
+        } else {
+            pcntl_signal(constant($name), $relay);
+        }
+    });
+
+    $relay_url = 'ws://127.0.0.1:' . $_SERVER['argv'][1];
+    
+    $websocket = new WebSocket\Client($relay_url);
+    $log = new Logger('agent');
+    $log->pushHandler(new StreamHandler(__DIR__ . '/logs/agent.log', Level::Debug));
+    $log->pushHandler(new StreamHandler(STDOUT), Level::Info);
+    $websocket->setLogger($log);
+    $agent = new \Transpher\WebSocket\Client($websocket);
     $agent->start();
 });
 echo 'Done';
--- src/Nostr/Server/Agent.php
+++ src/Nostr/Server/Agent.php
@@ -10,6 +10,6 @@ namespace Transpher\Nostr\Server;
 class Agent {
     static function boot(int $port, string $pubkey, array $env, callable $running) {
         $cmd = [PHP_BINARY, ROOT_DIR . DIRECTORY_SEPARATOR . 'agent.php', $port, $pubkey];
-        return \Transpher\Process::start('agent-' . $port, $cmd, $env, fn(string $line) => str_contains($line, 'Listening'), $running);
+        return \Transpher\Process::start('agent-' . $port, $cmd, $env, fn(string $line) => str_contains($line, 'Client connected to ws://127.0.0.1:' . $port), $running);
     }
 }

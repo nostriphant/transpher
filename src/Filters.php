b154c08bef706ce589fbe9e3bb8c9358b689665f
Rik Meijer <hello@rikmeijer.nl>
2024-09-10 21:35:33 +0200

    refactor

--- src/Filters.php
+++ src/Filters.php
@@ -13,10 +13,10 @@ class Filters {
     
     static function constructSubscription(array $filter_prototype) : callable {
         $subscription_filters = partial_left(Functional::some, array_map(fn(callable $possible_filter) => $possible_filter($filter_prototype), [
-            self::scalar('ids', 'id'),
-            self::scalar('kinds', 'kind'),
-            self::scalar('authors', 'pubkey'),
-            self::tag('#p', 'p')
+            self::make('ids', self::scalar('id')),
+            self::make('kinds', self::scalar('kind')),
+            self::make('authors', self::scalar('pubkey')),
+            self::make('#p', self::tag('p'))
         ]));
             
         return fn(array $event) => $subscription_filters(fn($subscription_filter) => $subscription_filter($event));
@@ -31,7 +31,7 @@ class Filters {
         return false;
     }
     
-    static function buildFilter(string $filter_field, callable $event_test) : callable {
+    static function make(string $filter_field, callable $event_test) : callable {
         return function(array $filters) use ($filter_field, $event_test) : callable {
             if (self::skipFilter($filters, $filter_field)) {
                 return fn() => false;
@@ -40,19 +40,11 @@ class Filters {
         };
     }
     
-    static function buildScalarTest(string|int $event_field) {
+    static function scalar(string|int $event_field) {
         return fn(array $filter_values, array $event) => in_array($event[$event_field], $filter_values);
     }
-    static function buildTagTest(string $event_tag_identifier) {
-        $tag_value_test = self::buildScalarTest(1);
+    static function tag(string $event_tag_identifier) {
+        $tag_value_test = self::scalar(1);
         return fn(array $filter_values, array $event) => some($event['tags'], fn(array $event_tag) => $event_tag[0] === $event_tag_identifier && $tag_value_test($filter_values, $event_tag));
-    }
-    
-    static function scalar(string $filter_field, string $event_field) : callable {
-        return self::buildFilter($filter_field, self::buildScalarTest($event_field));
-    }
-    
-    static function tag(string $filter_tag_identifier, string $event_tag_identifier) : callable {
-        return self::buildFilter($filter_tag_identifier, self::buildTagTest($event_tag_identifier));
     }
 }

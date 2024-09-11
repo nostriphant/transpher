557bb02773f2a3160f8e80b2ff461609710f7064
Rik Meijer <hello@rikmeijer.nl>
2024-09-11 20:42:31 +0200

    fix p tag not being set properly

--- tests/Feature/AgentTest.php
+++ tests/Feature/AgentTest.php
@@ -20,14 +20,14 @@ describe('agent', function() use ($main_key)  {
     $alice = \TranspherTests\Client::client(8084);
     $subscription = Transpher\Message::subscribe();
    
-//    it('starts relay and seeks connection with client', function () use ($alice, $subscription, $main_key) {
-//        
-//        $request = Transpher\Message::filter($subscription, tags: ['#p' => [$main_key()]])();
-//
-//        $alice->expectNostrPrivateDirectMessage($subscription()[1], 'Hello, I am Agent!');
-//        $alice->json($request);
-//        $alice->start();
-//    });
+    it('starts relay and seeks connection with client', function () use ($alice, $subscription, $main_key) {
+        
+        $request = Transpher\Message::filter($subscription, tags: ['#p' => [$main_key()]])();
+
+        $alice->expectNostrPrivateDirectMessage($subscription()[1], 'Hello, I am Agent!');
+        $alice->json($request);
+        $alice->start();
+    });
     
     
 });

cefc9b7bf55324416e89221add6fd8dcf71aff6e
Rik Meijer <hello@rikmeijer.nl>
2024-09-23 15:52:13 +0200

    simplify direct message code, because salt and keys must be rotated every message

--- src/Message.php
+++ src/Message.php
@@ -17,12 +17,10 @@ class Message {
     }
     
     
-    static function seal(callable $private_key, string $salt) : callable {
-        return function(array $direct_message, string $recipient_pubkey) use ($private_key, $salt) {
-            $conversation_key = self::getConversationKey($recipient_pubkey);
-            $encrypted_direct_message = Nostr\NIP44::encrypt(json_encode($direct_message), $private_key($conversation_key), $salt);
-            return Nostr::event($private_key, mktime(rand(0,23), rand(0,59), rand(0,59)), 1059, [], $encrypted_direct_message);
-        };
+    static function seal(callable $private_key, array $direct_message, string $salt, string $recipient_pubkey) {
+        $conversation_key = self::getConversationKey($recipient_pubkey);
+        $encrypted_direct_message = Nostr\NIP44::encrypt(json_encode($direct_message), $private_key($conversation_key), $salt);
+        return Nostr::event($private_key, mktime(rand(0,23), rand(0,59), rand(0,59)), 1059, [], $encrypted_direct_message);
     }
     
     static function getConversationKey(string $recipient_pubkey) : callable {
@@ -35,25 +33,20 @@ class Message {
         };
     }
     
-    static function giftWrap(callable $randomKey, string $salt) : callable {
-        return function(array $event, string $recipient_pubkey) use ($randomKey, $salt) {
-            $conversation_key = self::getConversationKey($recipient_pubkey);
-            $encrypted = Nostr\NIP44::encrypt(json_encode($event), $randomKey($conversation_key), $salt);
-            return Nostr::event($randomKey, mktime(rand(0,23), rand(0,59), rand(0,59)), 1059, ['p', $recipient_pubkey], $encrypted);
-        };
+    static function giftWrap(array $event, string $salt, string $recipient_pubkey) {
+        $randomKey = \Transpher\Key::generate();
+        $conversation_key = self::getConversationKey($recipient_pubkey);
+        $encrypted = Nostr\NIP44::encrypt(json_encode($event), $randomKey($conversation_key), $salt);
+        return Nostr::event($randomKey, mktime(rand(0,23), rand(0,59), rand(0,59)), 1059, ['p', $recipient_pubkey], $encrypted);
     }
     
-    static function privateDirect(callable $private_key, string ...$recipient_pubkeys) {
-        $salt = random_bytes(32);
-        $randomKey = \Transpher\Key::generate();
-        $wrap = self::giftWrap($randomKey, $salt);
-        $seal = self::seal($private_key, $salt);
-        return function(string $message) use ($private_key, $wrap, $seal, $recipient_pubkeys) {
-            $tags = map($recipient_pubkeys, fn(string $recipient_pubkey) => ['p', $recipient_pubkey]);
-            $unsigned_event = Message::event(14, $message, ...$tags);
+    static function privateDirect(callable $private_key, string $recipient_pubkey) {
+        return function(string $message) use ($private_key, $recipient_pubkey) {
+            $salt = random_bytes(32);
+            $unsigned_event = Message::event(14, $message, ['p', $recipient_pubkey]);
             $direct_message = $unsigned_event($private_key);
             unset($direct_message[1]['sig']);
-            return $wrap($seal($direct_message, $recipient_pubkeys[0]), $recipient_pubkeys[0]); //TODO: add support for multiple recipients
+            return self::giftWrap(self::seal($private_key, $direct_message, $salt, $recipient_pubkey), $salt, $recipient_pubkey);
         };
     }
     

80cdad17cdfd9c1d0685ad92fc6d71523f0fd299
Rik Meijer <hello@rikmeijer.nl>
2024-09-23 15:22:25 +0200

    formatting

--- tests/Feature/AgentTest.php
+++ tests/Feature/AgentTest.php
@@ -5,30 +5,26 @@ use Transpher\Nostr\Relay\Agent;
 $agent_key = \Transpher\Key::generate();
 $main_key = \Transpher\Key::generate();
 $main_agent;
-beforeAll(function() use (&$main_agent, $agent_key, $main_key) {
-    Agent::boot(8084, $main_key(), ['AGENT_KEY' => $agent_key(fn() => func_get_arg(0))], function(callable $agent) use (&$main_agent) {
+beforeAll(function () use (&$main_agent, $agent_key, $main_key) {
+    Agent::boot(8084, $main_key(), ['AGENT_KEY' => $agent_key(fn() => func_get_arg(0))], function (callable $agent) use (&$main_agent) {
         $main_agent = $agent;
     });
 });
-afterAll(function() use (&$main_agent) {
+afterAll(function () use (&$main_agent) {
     $status = $main_agent();
     expect($status)->toBeArray();
     expect($status['running'])->toBeFalse();
 });
 
-describe('agent', function() use ($main_key)  {
-     
+describe('agent', function () use ($main_key) {
+
     $alice = \TranspherTests\Client::client(8084);
     $subscription = Transpher\Message::subscribe();
-   
-    it('starts relay and seeks connection with client', function () use ($alice, $subscription, $main_key) {
-        
-        $request = Transpher\Message::filter($subscription, tags: [['#p' => [$main_key()]]])();
 
+    it('starts relay and seeks connection with client', function () use ($alice, $subscription, $main_key) {
+        $request = Transpher\Message::filter($subscription, tags: [['#p' => [$main_key()]]])();
         $alice->expectNostrPrivateDirectMessage($subscription()[1], $main_key, 'Hello, I am Agent!');
         $alice->json($request);
         $alice->start();
     });
-    
-    
 });

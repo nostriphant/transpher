e673d31fe39da212a02c5d8eb855cef004d4af80
Rik Meijer <hello@rikmeijer.nl>
2024-09-24 12:29:38 +0200

    wrap hash_* calls in Monad-like function

---
+++ src/HashSHA256.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Transpher;
+
+/**
+ * Description of Hash
+ *
+ * @author Rik Meijer <hello@rikmeijer.nl>
+ */
+readonly class HashSHA256 {
+    private \HashContext $context;
+    public function __construct(string $key) {
+        $this->context = hash_init('sha256', HASH_HMAC, $key);
+    }
+    public function __invoke(string $data) : self {
+        hash_update($this->context, $data);
+        return $this;
+    }
+    public function __toString() : string {
+        return hash_final($this->context, true);
+    }
+}

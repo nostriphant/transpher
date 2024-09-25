4db4624d86ca37443c6c98db9e9d56cd45e42094
Rik Meijer <hello@rikmeijer.nl>
2024-09-25 20:56:34 +0200

    make Process a callable object

--- src/Process.php
+++ src/Process.php
@@ -7,41 +7,45 @@ namespace Transpher;
  *
  * @author Rik Meijer <hello@rikmeijer.nl>
  */
-class Process {
-    static function start(string $process_id, array $cmd, array $env, callable $runtest, callable $running) : void {
+readonly class Process {
+    
+    private mixed $process;
+    
+    public function __construct(string $process_id, array $cmd, array $env, callable $runtest) {
         $cwd = getcwd();
-        
-        $output_file = $cwd . "/logs/{$process_id}-errors.log";
-        
+        $output_file = $cwd . "/logs/{$process_id}-output.log";
         $descriptorspec = [
             0 => ["pipe", "r"],  // stdin is a pipe that the child will read from
-            1 => ["file", $cwd . "/logs/{$process_id}-output.log", "w"],  // stdout is a pipe that the child will write to
+            1 => ["file", $output_file, "w"],  // stdout is a pipe that the child will write to
             2 => ["file", $cwd . "/logs/{$process_id}-errors.log", "w"] // stderr is a file to write to
         ];
 
-        $process = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
-        
-        while ($runtest(file_get_contents($cwd . "/logs/{$process_id}-output.log")) === false) {
+        $this->process = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
+        while ($runtest(file_get_contents($output_file)) === false) {
             // wait till process is ready
         }
         
         
-        if ($process === false) {
-            
-        } elseif (is_resource($process)) {
+        if ($this->process === false) {
+        } elseif (is_resource($this->process)) {
             fclose($pipes[0]);
-            
-            $running(function(int $signal = 15) use ($process_id, $process, $pipes, $output_file) : array {
-                $status = proc_get_status($process);
-                
-                proc_terminate($process, $signal);
-                while ($status['running']) {
-                    $status = proc_get_status($process);
-                }
+        }   
+    }
+    
+    public function __invoke(int $signal = 15) : array {
+        $status = proc_get_status($this->process);
 
-                proc_close($process);
-                return $status;
-            });
+        proc_terminate($this->process, $signal);
+        while ($status['running']) {
+            $status = proc_get_status($this->process);
         }
+
+        proc_close($this->process);
+        return $status;
+    }
+    
+    static function start(string $process_id, array $cmd, array $env, callable $runtest, callable $running) : void {
+        $process = new static($process_id, $cmd, $env, $runtest);
+        $running($process);
     }
 }

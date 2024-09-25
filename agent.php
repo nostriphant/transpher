7671a8c1deabbbe715908de4cc06c90e5590048e
Rik Meijer <hello@rikmeijer.nl>
2024-09-25 15:32:39 +0200

    write public key to log, so owner can find agent

--- agent.php
+++ agent.php
@@ -27,9 +27,13 @@ $port = $_SERVER['argv'][1] ?? 80;
     $log = new Logger('agent');
     $log->pushHandler(new StreamHandler(__DIR__ . '/logs/agent.log', Level::Debug));
     $log->pushHandler(new StreamHandler(STDOUT), Level::Info);
+    
+    $agent_key = Key::fromBech32($_SERVER['AGENT_NSEC']);
+    $log->info('Running agent with public key ' . $agent_key(Key::public(\Transpher\Nostr\Key\Format::BECH32)));
+    
     $agent = new \Transpher\WebSocket\Client(new WebSocket\Client($relay_url), $log);
     $log->info('Sending Private Direct Message event');
-    $note = Message::privateDirect(Key::fromBech32($_SERVER['AGENT_NSEC']));
+    $note = Message::privateDirect($agent_key);
     $agent->json($note(Key::convertBech32ToHex($_SERVER['AGENT_OWNER_NPUB']), 'Hello, I am Agent!'));
     $agent->start();
 });

9684bfa8baca6deef5fb01cca57a344652d90578
Rik Meijer <hello@rikmeijer.nl>
2024-09-30 12:35:09 +0200

    use match statement to reduce code

--- src/WebSocket/Server.php
+++ src/WebSocket/Server.php
@@ -33,13 +33,9 @@ class Server {
     }
     
     public function __invoke(Connection $from, callable $reply, array $others, array $payload) {
-        $edit_subscriptions = self::subscriptionEditor($from);
-
-        $relay_event_to_subscribers = self::eventRelayer($this->events, $others);
-
         $subscription_handler = fn() => yield from match(func_num_args()) {
-            1 => $relay_event_to_subscribers(func_get_arg(0)),
-            default => $edit_subscriptions($this->events, ...func_get_args())
+            1 => self::relay($this->events, $others, func_get_arg(0)),
+            default => self::subscribe($from, $this->events, ...func_get_args())
         };
 
         foreach(NServer::listen($payload, $subscription_handler) as $reply_message) {
@@ -48,24 +44,20 @@ class Server {
     }
     
     
-    static function subscriptionEditor(Connection $from) {
-        return function(array|\ArrayAccess &$events, string $subscriptionId, ?callable $subscription) use ($from) {
-            $subscriptions = $from->getMeta('subscriptions')??[];
-            if (is_null($subscription)) {
-                unset($subscriptions[$subscriptionId]);
-            } else {
-                $subscriptions[$subscriptionId] = $subscription;
-                yield from map(filter($events, $subscription), fn(array $event) => Message::requestedEvent($subscriptionId, $event));
-            }
-            $from->setMeta('subscriptions', $subscriptions);
-        };
+    static function subscribe(Connection $from, array|\ArrayAccess &$events, string $subscriptionId, ?callable $subscription) {
+        $subscriptions = $from->getMeta('subscriptions')??[];
+        if (is_null($subscription)) {
+            unset($subscriptions[$subscriptionId]);
+        } else {
+            $subscriptions[$subscriptionId] = $subscription;
+            yield from map(filter($events, $subscription), fn(array $event) => Message::requestedEvent($subscriptionId, $event));
+        }
+        $from->setMeta('subscriptions', $subscriptions);
     }
     
-    static function eventRelayer(array|\ArrayAccess &$events, array $others) {
-        return function(array $event) use (&$events, $others) : array {
-            $events[] = $event;
-            each($others, fn(callable $other) => $other($event));
-            return [];
-        };
+    static function relay(array|\ArrayAccess &$events, array $others, array $event) : array {
+        $events[] = $event;
+        each($others, fn(callable $other) => $other($event));
+        return [];
     }
 }

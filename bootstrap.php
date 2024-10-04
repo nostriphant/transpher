fe22bd1c0adc9657b9246d3981388b7c4bcb3012
Rik Meijer <hello@rikmeijer.nl>
2024-10-04 14:46:03 +0200

    prevent missing .env, because assertions have been disabled

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 define('ROOT_DIR', __DIR__);
-
-assert(is_dir(ROOT_DIR . '/logs') || mkdir(ROOT_DIR . '/logs'));
+is_dir(ROOT_DIR . '/logs') || mkdir(ROOT_DIR . '/logs');
 
 $dotenv_file = ROOT_DIR . '/.env';
-assert(file_exists($dotenv_file) || touch($dotenv_file), 'no env set');
+is_file($dotenv_file) || touch($dotenv_file);
 $dotenv = Dotenv\Dotenv::createMutable(dirname($dotenv_file));
 $dotenv->load();

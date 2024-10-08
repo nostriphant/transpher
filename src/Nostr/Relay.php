70962767d46f8005165ecca959a58504cfdce91a
Rik Meijer <hello@rikmeijer.nl>
2024-10-08 18:11:18 +0200

    refactor

--- src/Nostr/Relay.php
+++ src/Nostr/Relay.php
@@ -30,46 +30,45 @@ class Relay {
         $message = \Transpher\Nostr::decode($payload);
         if (is_null($message)) {
             yield Message::notice('Invalid message');
-            return;
-        }
-        
-        $type = array_shift($message);
-        switch (strtoupper($type)) {
-            case 'EVENT': 
-                $this->events[] = $message[0];
-                $store($message[0]);
-                yield Message::accept($message[0]['id']);
-                break;
-            
-            case 'CLOSE': 
-                if (count($message) < 1) {
-                    yield Message::notice('Missing subscription ID');
-                } else {
-                    Subscriptions::unsubscribe($message[0]);
-                    yield Message::closed($message[0]);
-                }
-                break;
-            
-            case 'REQ':
-                if (count($message) < 2) {
-                    yield Message::notice('Invalid message');
-                } elseif (empty($message[1])) {
-                    yield Message::closed($message[0], 'Subscription filters are empty');
-                } else {
-                    $subscription = Filters::constructFromPrototype($message[1]);
-                    Subscriptions::subscribe($message[0], $subscription, function(string $subscriptionId, array $event) use ($relay) : bool {
-                        $relay(\Transpher\Nostr\Message::requestedEvent($subscriptionId, $event));
-                        $relay(\Transpher\Nostr\Message::eose($subscriptionId));
-                        return true;
-                    });
-                    yield from map(filter($this->events, $subscription), fn(array $event) => Message::requestedEvent($message[0], $event));
-                    yield Message::eose($message[0]);
-                }
-                break;
-                
-            default: 
-                yield Message::notice('Message type ' . $type . ' not supported');
-                break;
+        } else {
+            $type = array_shift($message);
+            switch (strtoupper($type)) {
+                case 'EVENT': 
+                    $this->events[] = $message[0];
+                    $store($message[0]);
+                    yield Message::accept($message[0]['id']);
+                    break;
+
+                case 'CLOSE': 
+                    if (count($message) < 1) {
+                        yield Message::notice('Missing subscription ID');
+                    } else {
+                        Subscriptions::unsubscribe($message[0]);
+                        yield Message::closed($message[0]);
+                    }
+                    break;
+
+                case 'REQ':
+                    if (count($message) < 2) {
+                        yield Message::notice('Invalid message');
+                    } elseif (empty($message[1])) {
+                        yield Message::closed($message[0], 'Subscription filters are empty');
+                    } else {
+                        $subscription = Filters::constructFromPrototype($message[1]);
+                        Subscriptions::subscribe($message[0], $subscription, function(string $subscriptionId, array $event) use ($relay) : bool {
+                            $relay(\Transpher\Nostr\Message::requestedEvent($subscriptionId, $event));
+                            $relay(\Transpher\Nostr\Message::eose($subscriptionId));
+                            return true;
+                        });
+                        yield from map(filter($this->events, $subscription), fn(array $event) => Message::requestedEvent($message[0], $event));
+                        yield Message::eose($message[0]);
+                    }
+                    break;
+
+                default: 
+                    yield Message::notice('Message type ' . $type . ' not supported');
+                    break;
+            }
         }
     }
 }

7aa263bdd4a86515e5477f9a1c18dde8792db87f
Rik Meijer <hello@rikmeijer.nl>
2024-10-09 12:22:18 +0200

    construct relay in Subscriptions class

--- src/Nostr/Relay/Subscriptions.php
+++ src/Nostr/Relay/Subscriptions.php
@@ -3,7 +3,7 @@
 namespace Transpher\Nostr\Relay;
 
 use Functional\Functional;
-use function \Functional\if_else, \Functional\first, \Functional\not, \Functional\identical, \Functional\partial_left;
+use function \Functional\if_else, \Functional\first;
 use Transpher\Nostr\Message;
 
 /**
@@ -16,16 +16,21 @@ use Transpher\Nostr\Message;
     private static array $subscriptions = [];
 
     static function apply(array $event): bool {
-        return empty(self::$subscriptions) ? false : null !== first(self::$subscriptions, fn(array $subscription) => $subscription[0]($event));
+        if (empty(self::$subscriptions)) {
+            return false;
+        }
+        return null !== first(self::$subscriptions, function(callable $subscription, string $subscriptionId) use ($event) {
+            $to = $subscription($event);
+            if ($to === false) {
+                return false;
+            }
+            $to(Message::requestedEvent($subscriptionId, $event));
+            $to(Message::eose($subscriptionId));
+            return true;
+        });
     }
     static function subscribe(string $subscriptionId, Filter $matcher, callable $relay) : void {
-        $success = function(string $subscriptionId, array $event) use ($relay) : bool {
-            $relay(Message::requestedEvent($subscriptionId, $event));
-            $relay(Message::eose($subscriptionId));
-            return true;
-        };
-        $subscription_relay = if_else($matcher, fn($event) => $success($subscriptionId, $event), Functional::false);
-        self::$subscriptions[$subscriptionId] = [$subscription_relay];
+        self::$subscriptions[$subscriptionId] = if_else($matcher, fn() => $relay, Functional::false);
     }
     static function unsubscribe(string $subscriptionId) : void {
         unset(self::$subscriptions[$subscriptionId]);

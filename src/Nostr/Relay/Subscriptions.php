6d57018b5da7b7f1b016e44d7be8f89a4a641832
Rik Meijer <hello@rikmeijer.nl>
2024-10-09 12:01:06 +0200

    use subscription-id as key in subscriptions array again

--- src/Nostr/Relay/Subscriptions.php
+++ src/Nostr/Relay/Subscriptions.php
@@ -15,14 +15,14 @@ use function \Functional\if_else, \Functional\first, \Functional\not, \Functiona
     private static array $subscriptions = [];
 
     static function apply(array $event): bool {
-        return empty(self::$subscriptions) ? false : null !== first(self::$subscriptions, fn(array $subscription) => $subscription[1]($event));
+        return empty(self::$subscriptions) ? false : null !== first(self::$subscriptions, fn(array $subscription) => $subscription[0]($event));
     }
     static function subscribe(string $subscriptionId, Filter $matcher, callable $success) : void {
         $subscription_relay = if_else($matcher, fn($event) => $success($subscriptionId, $event), Functional::false);
-        self::$subscriptions[] = [$subscriptionId, $subscription_relay];
+        self::$subscriptions[$subscriptionId] = [$subscription_relay];
     }
     static function unsubscribe(string $subscriptionId) : void {
-        self::$subscriptions = array_filter(self::$subscriptions, fn(array $item) => $item[0] !== $subscriptionId);
+        unset(self::$subscriptions[$subscriptionId]);
     }
     
 }

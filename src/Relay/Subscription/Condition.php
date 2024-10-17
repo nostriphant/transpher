137c70bf49ff7475543a4dae9290da3a5eeeec23
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-17 10:12:52 +0200

    refactor

--- src/Relay/Subscription/Condition.php
+++ src/Relay/Subscription/Condition.php
@@ -47,7 +47,7 @@ class Condition {
     static function map(array $filter_prototypes) {
         return map($filter_prototypes, function (array $filter_prototype) {
             $available_conditions = [];
-            foreach (glob(__DIR__ . '/Conditions/*.php') as $available_filter_file) {
+            foreach (glob(__DIR__ . '/Condition/*.php') as $available_filter_file) {
                 $available_conditions[basename($available_filter_file, '.php')] = $available_filter_file;
             }
             return map(array_intersect_key($filter_prototype, $available_conditions), fn($condition, $filter_field) => (require $available_conditions[$filter_field])($condition));

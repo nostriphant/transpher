9c0faab6ed16dcfc20df6da32ae6cf16ecc1f09b
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-18 16:18:49 +0200

    move all API tests to faster Unit tests

--- tests/Unit/RelayTest.php
+++ tests/Unit/RelayTest.php
@@ -3,6 +3,11 @@
 use rikmeijer\Transpher\Nostr\Key;
 use rikmeijer\Transpher\Nostr\Message\Factory;
 use rikmeijer\Transpher\Relay\InformationDocument;
+use rikmeijer\Transpher\Nostr\Subscription\Filter;
+use rikmeijer\Transpher\Nostr\Filters;
+use function \Functional\select,
+             \Functional\map,
+             \Functional\partial_left;
 
 it('generates a NIP11 Relay Information Document', function() {
     
@@ -24,35 +29,85 @@ it('generates a NIP11 Relay Information Document', function() {
 });
 
 class Client extends \rikmeijer\TranspherTests\Client {
-    
+
+    static rikmeijer\Transpher\Relay $generic_relay;
     private $messages = [];
-    
-    public function __construct() {
+
+    public function __construct(private rikmeijer\Transpher\Relay $relay) {
         
     }
-    static function generic_client() : self {
-        return new self();
+
+    static function persistent_client(string $store): self {
+        return new self(new \rikmeijer\Transpher\Relay(new \rikmeijer\Transpher\Directory($store)));
     }
 
+    static function generic_client(): self {
+        if (isset(self::$generic_relay) === false) {
+            $events = new class implements rikmeijer\Transpher\Relay\Store {
 
+                private array $events = [];
+
+                #[\Override]
+                public function __invoke(Filters $subscription): callable {
+                    return fn(string $subscriptionId) => map(select($this->events, $subscription), partial_left([Factory::class, 'requestedEvent'], $subscriptionId));
+                }
+
+                #[\Override]
+                public function offsetExists(mixed $offset): bool {
+                    return isset($this->events[$offset]);
+                }
+
+                #[\Override]
+                public function offsetGet(mixed $offset): mixed {
+                    return $this->events[$offset];
+                }
+
+                #[\Override]
+                public function offsetSet(mixed $offset, mixed $value): void {
+                    if (isset($offset)) {
+                        $this->events[$offset] = $value;
+                    } else {
+                        $this->events[] = $value;
+                    }
+                }
+
+                #[\Override]
+                public function offsetUnset(mixed $offset): void {
+                    unset($this->events[$offset]);
+                }
+            };
+            self::$generic_relay = new \rikmeijer\Transpher\Relay($events);
+        }
+        return new self(self::$generic_relay);
+    }
+
+    public function relay(mixed $json) {
+        $this->messages[] = $json;
+    }
+
+    #[\Override]
     public function send(string $text): void {
-        $events = Mockery::mock(rikmeijer\Transpher\Relay\Store::class)->allows([
-            'offsetSet' => null
-        ]);
-        $relay = new \rikmeijer\Transpher\Relay($events);
+        $relayer = new class($this) implements \rikmeijer\Transpher\Relay\Sender {
 
-        $relayer = Mockery::mock(\rikmeijer\Transpher\Relay\Sender::class)->allows([
-            '__invoke' => true
-        ]);
+            public function __construct(private Client $client) {
 
-        foreach ($relay($text, $relayer) as $response) {
+            }
+
+            #[\Override]
+            public function __invoke(mixed $json): bool {
+                $this->client->relay($json);
+                return true;
+            }
+        };
+
+        foreach (($this->relay)($text, $relayer) as $response) {
             $this->messages[] = $response;
         }
     }
 
     #[\Override]
     public function receive() : Amp\Websocket\WebsocketMessage {
-        return Amp\Websocket\WebsocketMessage::fromText(array_shift($this->messages));
+        return Amp\Websocket\WebsocketMessage::fromText(array_shift($this->messages) ?? '');
     }
     
 }
@@ -125,5 +180,171 @@ it('replies CLOSED on empty filters', function () {
     $request = $subscription();
     $request[] = [];
     $bob->json($request);
+    $bob->start();
+});
+
+
+it('sends events to all clients subscribed on event id', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+
+    $alice_key = Key::generate();
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello worlda!'));
+
+    $key_charlie = Key::generate();
+    $note2 = Factory::event($key_charlie, 1, 'Hello worldi!');
+    $alice->sendSignedMessage($note2);
+
+    $subscription = Factory::subscribe(
+            new Filter(ids: [$note2()[1]['id']])
+    );
+
+    $bob->expectNostrEvent($subscription()[1], 'Hello worldi!');
+    $bob->expectNostrEose($subscription()[1]);
+    $bob->json($subscription());
+    $bob->start();
+});
+
+
+it('sends events to all clients subscribed on author (pubkey)', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+
+    $alice_key = Key::generate();
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello world!'));
+    $subscription = Factory::subscribe(
+            new Filter(authors: [$alice_key(Key::public())])
+    );
+
+    $bob->expectNostrEvent($subscription()[1], 'Hello world!');
+    $bob->expectNostrEose($subscription()[1]);
+
+    $bob->json($subscription());
+    $bob->start();
+});
+
+it('sends events to Charly who uses two filters in their subscription', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+    $charlie = Client::generic_client();
+
+    $alice_key = Key::generate();
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello world, from Alice!'));
+
+    $bob_key = Key::generate();
+    $alice->sendSignedMessage(Factory::event($bob_key, 1, 'Hello world, from Bob!'));
+
+    $subscription = Factory::subscribe(
+            new Filter(authors: [$alice_key(Key::public())]),
+            new Filter(authors: [$bob_key(Key::public())])
+    );
+    $charlie->expectNostrEvent($subscription()[1], 'Hello world, from Alice!');
+    $charlie->expectNostrEvent($subscription()[1], 'Hello world, from Bob!');
+    $charlie->expectNostrEose($subscription()[1]);
+
+    $charlie->json($subscription());
+    $charlie->start();
+});
+
+it('sends events to all clients subscribed on p-tag', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+    $alice_key = Key::generate();
+
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello world!', ['p', 'randomPTag']));
+    $subscription = Factory::subscribe(
+            new Filter(tags: ['#p' => ['randomPTag']])
+    );
+
+    $bob->expectNostrEvent($subscription()[1], 'Hello world!');
+    $bob->expectNostrEose($subscription()[1]);
+
+    $bob->json($subscription());
+    $bob->start();
+});
+
+it('closes subscription and stop sending events to subscribers', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+    $alice_key = Key::generate();
+
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello world!'));
+
+    $subscription = Factory::subscribe(
+            new Filter(authors: [$alice_key(Key::public())])
+    );
+    $bob->expectNostrEvent($subscription()[1], 'Hello world!');
+    $bob->expectNostrEose($subscription()[1]);
+
+    $bob->json($subscription());
+    $bob->start();
+
+    $bob->expectNostrClosed($subscription()[1], '');
+
+    $request = Factory::close($subscription()[1]);
+    $bob->send($request);
+    $bob->start();
+});
+
+
+it('sends events to all clients subscribed on kind', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+    $alice_key = Key::generate();
+
+    $alice->sendSignedMessage(Factory::event($alice_key, 3, 'Hello world!'));
+
+    $subscription = Factory::subscribe(
+            new Filter(kinds: [3])
+    );
+
+    $bob->expectNostrEvent($subscription()[1], 'Hello world!');
+    $bob->expectNostrEose($subscription()[1]);
+
+    $bob->json($subscription());
+    $bob->start();
+});
+
+it('relays events to Bob, sent after they subscribed on Alices messages', function () {
+    $alice = Client::generic_client();
+    $bob = Client::generic_client();
+    $alice_key = Key::generate();
+
+    $subscription = Factory::subscribe(
+            new Filter(authors: [$alice_key(Key::public())])
+    );
+
+    $bob->expectNostrEose($subscription()[1]);
+    $bob->json($subscription());
+    $bob->start();
+
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Relayable Hello worlda!'));
+
+    $key_charlie = Key::generate();
+    $alice->sendSignedMessage(Factory::event($key_charlie, 1, 'Hello worldi!'));
+
+    $bob->expectNostrEvent($subscription()[1], 'Relayable Hello worlda!');
+    $bob->expectNostrEose($subscription()[1]);
+    $bob->start();
+});
+
+it('sends events to all clients subscribed on author (pubkey), even after restarting the server', function () {
+    $transpher_store = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid();
+    mkdir($transpher_store);
+
+    $alice = Client::persistent_client($transpher_store);
+
+    $alice_key = Key::generate();
+    $alice->sendSignedMessage(Factory::event($alice_key, 1, 'Hello wirld!'));
+
+    $bob = Client::persistent_client($transpher_store);
+    $subscription = Factory::subscribe(
+            new Filter(authors: [$alice_key(Key::public())])
+    );
+
+    $bob->expectNostrEvent($subscription()[1], 'Hello wirld!');
+    $bob->expectNostrEose($subscription()[1]);
+
+    $bob->json($subscription());
     $bob->start();
 });

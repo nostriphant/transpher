ff47388076a092db99b95132e8494b1883a6b17f
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-18 16:22:23 +0200

    add Unit tests Nostr Client

--- tests/Unit/RelayTest.php
+++ tests/Unit/RelayTest.php
@@ -4,10 +4,7 @@ use rikmeijer\Transpher\Nostr\Key;
 use rikmeijer\Transpher\Nostr\Message\Factory;
 use rikmeijer\Transpher\Relay\InformationDocument;
 use rikmeijer\Transpher\Nostr\Subscription\Filter;
-use rikmeijer\Transpher\Nostr\Filters;
-use function \Functional\select,
-             \Functional\map,
-             \Functional\partial_left;
+use rikmeijer\TranspherTests\Unit\Client;
 
 it('generates a NIP11 Relay Information Document', function() {
     
@@ -27,90 +24,6 @@ it('generates a NIP11 Relay Information Document', function() {
     ]);
     
 });
-
-class Client extends \rikmeijer\TranspherTests\Client {
-
-    static rikmeijer\Transpher\Relay $generic_relay;
-    private $messages = [];
-
-    public function __construct(private rikmeijer\Transpher\Relay $relay) {
-        
-    }
-
-    static function persistent_client(string $store): self {
-        return new self(new \rikmeijer\Transpher\Relay(new \rikmeijer\Transpher\Directory($store)));
-    }
-
-    static function generic_client(): self {
-        if (isset(self::$generic_relay) === false) {
-            $events = new class implements rikmeijer\Transpher\Relay\Store {
-
-                private array $events = [];
-
-                #[\Override]
-                public function __invoke(Filters $subscription): callable {
-                    return fn(string $subscriptionId) => map(select($this->events, $subscription), partial_left([Factory::class, 'requestedEvent'], $subscriptionId));
-                }
-
-                #[\Override]
-                public function offsetExists(mixed $offset): bool {
-                    return isset($this->events[$offset]);
-                }
-
-                #[\Override]
-                public function offsetGet(mixed $offset): mixed {
-                    return $this->events[$offset];
-                }
-
-                #[\Override]
-                public function offsetSet(mixed $offset, mixed $value): void {
-                    if (isset($offset)) {
-                        $this->events[$offset] = $value;
-                    } else {
-                        $this->events[] = $value;
-                    }
-                }
-
-                #[\Override]
-                public function offsetUnset(mixed $offset): void {
-                    unset($this->events[$offset]);
-                }
-            };
-            self::$generic_relay = new \rikmeijer\Transpher\Relay($events);
-        }
-        return new self(self::$generic_relay);
-    }
-
-    public function relay(mixed $json) {
-        $this->messages[] = $json;
-    }
-
-    #[\Override]
-    public function send(string $text): void {
-        $relayer = new class($this) implements \rikmeijer\Transpher\Relay\Sender {
-
-            public function __construct(private Client $client) {
-
-            }
-
-            #[\Override]
-            public function __invoke(mixed $json): bool {
-                $this->client->relay($json);
-                return true;
-            }
-        };
-
-        foreach (($this->relay)($text, $relayer) as $response) {
-            $this->messages[] = $response;
-        }
-    }
-
-    #[\Override]
-    public function receive() : Amp\Websocket\WebsocketMessage {
-        return Amp\Websocket\WebsocketMessage::fromText(array_shift($this->messages) ?? '');
-    }
-    
-}
 
 it('responds with a NOTICE on null message', function () {
     $alice = Client::generic_client();

92791c5f03914473444b05b2b4b0579cee18e7e6
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-22 11:10:55 +0200

    it does not store ephemeral events

--- tests/Unit/Relay/Incoming/EventTest.php
+++ tests/Unit/Relay/Incoming/EventTest.php
@@ -18,5 +18,20 @@ describe('event storing', function () {
         expect($events)->toHaveCount(1);
         expect(isset($events['my-event']))->toBeTrue();
     });
+
+    it('stores no ephemeral events', function () {
+        $events = new class([]) implements rikmeijer\Transpher\Relay\Store {
+
+            use \rikmeijer\Transpher\Nostr\EventsStore;
+        };
+        $incoming = new \rikmeijer\Transpher\Relay\Incoming\Event(Functions::event(['kind' => 20000, 'id' => 'my-event']));
+        $event = $incoming();
+        expect($events)->toHaveCount(0);
+        expect($events)->not()->toHaveKey('my-event');
+        foreach ($event($events) as $message) {
+
+        }
+        expect($events)->toHaveCount(0);
+    });
 });
 

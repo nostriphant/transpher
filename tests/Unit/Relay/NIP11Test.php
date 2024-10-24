3ced55abbbd3a59f740d8b178b96e0e377ee1ab0
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-24 11:59:44 +0200

    move NIP11 test to own file

---
+++ tests/Unit/Relay/NIP11Test.php
@@ -0,0 +1,21 @@
+<?php
+
+use rikmeijer\Transpher\Relay\InformationDocument;
+
+it('generates Relay Information Document', function () {
+
+    $name = 'Transpher Relay';
+    $description = 'Some interesting description goes here';
+    $owner_npub = 'npub1cza3sx7rn389ja5gqkaut0wnf3gg799srg5c6ca7g5gdjaqhecqsg485p4';
+    $contact = 'nostr@rikmeijer.nl';
+
+    expect(InformationDocument::generate($name, $description, $owner_npub, $contact))->toBe([
+        "name" => 'Transpher Relay',
+        "description" => 'Some interesting description goes here',
+        "pubkey" => 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01',
+        "contact" => 'nostr@rikmeijer.nl',
+        "supported_nips" => [1, 11],
+        "software" => 'Transpher',
+        "version" => 'dev'
+    ]);
+});
--- tests/Unit/RelayTest.php
+++ tests/Unit/RelayTest.php
@@ -2,28 +2,9 @@
 
 use rikmeijer\Transpher\Nostr\Key;
 use rikmeijer\Transpher\Nostr\Message\Factory;
-use rikmeijer\Transpher\Relay\InformationDocument;
 use rikmeijer\Transpher\Nostr\Subscription\Filter;
 use rikmeijer\TranspherTests\Unit\Client;
 
-it('generates a NIP11 Relay Information Document', function() {
-    
-    $name = 'Transpher Relay';
-    $description = 'Some interesting description goes here';
-    $owner_npub = 'npub1cza3sx7rn389ja5gqkaut0wnf3gg799srg5c6ca7g5gdjaqhecqsg485p4';
-    $contact = 'nostr@rikmeijer.nl';
-    
-    expect(InformationDocument::generate($name, $description, $owner_npub, $contact))->toBe([
-        "name" => 'Transpher Relay',
-        "description" => 'Some interesting description goes here',
-        "pubkey" => 'c0bb181bc39c4e59768805bbc5bdd34c508f14b01a298d63be4510d97417ce01',
-        "contact" => 'nostr@rikmeijer.nl',
-        "supported_nips" => [1, 11],
-        "software" => 'Transpher',
-        "version" => 'dev'
-    ]);
-    
-});
 
 it('sends events to all clients subscribed on event id', function () {
     $alice = Client::generic_client();

cab187efc28222f6579daababeeb291da7fe3250
Rik Meijer (Weman Rik) <hello@rikmeijer.nl>
2024-10-25 09:41:07 +0200

    make NIP33 support (addressable event kinds) explicit

--- tests/Unit/Relay/NIP01/KindsTest.php
+++ tests/Unit/Relay/NIP01/KindsTest.php
@@ -20,47 +20,4 @@ describe('Kinds (https://nips.nostr.com/1#kinds)', function () {
         );
     });
 
-    it('replaces addressable (30000 <= n < 40000) events, keeping only the last one (based on pubkey, kind and d)', function () {
-        $context = context();
-
-        $sender_key = Key::generate();
-        for ($kind = 30000; $kind < 40000; $kind += rand(100, 5000)) {
-            $original_event = Factory::event($sender_key, $kind, 'Hello World', ['d', 'my-d-tag-value']);
-            Relay::handle($original_event, $context);
-
-            expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
-
-            $updated_event = Factory::eventAt($sender_key, $kind, 'Updated: hello World', time() + 100, ['d', 'my-d-tag-value']);
-            Relay::handle($updated_event, $context);
-
-            expect(isset($context->events[$original_event()[1]['id']]))->ToBeFalse();
-            expect(isset($context->events[$updated_event()[1]['id']]))->toBeTrue();
-        }
-    });
-    it('keeps addressable (30000 <= n < 40000) events, when same created_at with lowest id (based on pubkey, kind and d)', function () {
-        $context = context();
-
-        $sender_key = Key::generate();
-        for ($kind = 30000; $kind < 40000; $kind += 5000) {
-            $time = time();
-            $event1 = Factory::eventAt($sender_key, $kind, 'Hello World', $time, ['d', 'my-d-tag-value']);
-            $event2 = Factory::eventAt($sender_key, $kind, 'Updated: hello World', $time, ['d', 'my-d-tag-value']);
-            if ($event1()[1]['id'] < $event2()[1]['id']) {
-                $original_event = $event1;
-                $updated_event = $event2;
-            } else {
-                $original_event = $event2;
-                $updated_event = $event1;
-            }
-
-            Relay::handle($original_event, $context);
-
-            expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
-
-            Relay::handle($updated_event, $context);
-
-            expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
-            expect(isset($context->events[$updated_event()[1]['id']]))->toBeFalse();
-        }
-    });
 });
---
+++ tests/Unit/Relay/NIP33Test.php
@@ -0,0 +1,53 @@
+<?php
+
+use rikmeijer\Transpher\Relay;
+use rikmeijer\Transpher\Nostr\Key;
+use rikmeijer\Transpher\Nostr\Message\Factory;
+use function Pest\context;
+
+/**
+ * https://github.com/nostr-protocol/nips/commit/72bb8a128b2d7d3c2c654644cd68d0d0fe58a3b1#diff-13387ea5b220b50dc20daca792c07b3b3d2b278dcafc8f134e4369468cab6440
+ */
+it('replaces addressable (30000 <= n < 40000) events, keeping only the last one (based on pubkey, kind and d)', function () {
+    $context = context();
+
+    $sender_key = Key::generate();
+    for ($kind = 30000; $kind < 40000; $kind += rand(100, 5000)) {
+        $original_event = Factory::event($sender_key, $kind, 'Hello World', ['d', 'my-d-tag-value']);
+        Relay::handle($original_event, $context);
+
+        expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
+
+        $updated_event = Factory::eventAt($sender_key, $kind, 'Updated: hello World', time() + 100, ['d', 'my-d-tag-value']);
+        Relay::handle($updated_event, $context);
+
+        expect(isset($context->events[$original_event()[1]['id']]))->ToBeFalse();
+        expect(isset($context->events[$updated_event()[1]['id']]))->toBeTrue();
+    }
+});
+it('keeps addressable (30000 <= n < 40000) events, when same created_at with lowest id (based on pubkey, kind and d)', function () {
+    $context = context();
+
+    $sender_key = Key::generate();
+    for ($kind = 30000; $kind < 40000; $kind += 5000) {
+        $time = time();
+        $event1 = Factory::eventAt($sender_key, $kind, 'Hello World', $time, ['d', 'my-d-tag-value']);
+        $event2 = Factory::eventAt($sender_key, $kind, 'Updated: hello World', $time, ['d', 'my-d-tag-value']);
+        if ($event1()[1]['id'] < $event2()[1]['id']) {
+            $original_event = $event1;
+            $updated_event = $event2;
+        } else {
+            $original_event = $event2;
+            $updated_event = $event1;
+        }
+
+        Relay::handle($original_event, $context);
+
+        expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
+
+        Relay::handle($updated_event, $context);
+
+        expect(isset($context->events[$original_event()[1]['id']]))->toBeTrue();
+        expect(isset($context->events[$updated_event()[1]['id']]))->toBeFalse();
+    }
+});

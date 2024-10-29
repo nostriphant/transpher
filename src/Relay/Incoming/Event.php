9b7f96e4f2c13dd709ae7bdbc788b952275fef7d
Rik Meijer <rik@nostriphant.dev>
2024-10-29 20:30:46 +0100

    add support for deleting events by kind (k) referencing

--- src/Relay/Incoming/Event.php
+++ src/Relay/Incoming/Event.php
@@ -29,11 +29,26 @@ readonly class Event implements \rikmeijer\Transpher\Relay\Incoming {
                     $context->events[$this->event->id] = $this->event;
                     switch ($this->event->kind) {
                         case 5:
-                            $event_ids = array_map(fn(array $tag) => $tag[1], array_filter($this->event->tags, fn(array $tag) => $tag[0] === 'e'));
-                            $removable_events = ($context->events)(Condition::makeFiltersFromPrototypes([
-                                        'authors' => [$this->event->pubkey],
-                                        'ids' => $event_ids
-                            ]));
+                            $prototypes = [];
+                            $possible_references = [
+                                'e' => 'ids',
+                                'k' => 'kinds'
+                            ];
+                            foreach ($possible_references as $possible_reference => $possible_filter_field) {
+                                $values = array_filter($this->event->tags, fn(array $tag) => $tag[0] === $possible_reference);
+                                if (empty($values)) {
+                                    continue;
+                                }
+
+                                $prototypes[] = [
+                                    'authors' => [$this->event->pubkey],
+                                    $possible_filter_field => array_map(fn(array $tag) => $tag[1], $values)
+                                ];
+                            }
+                            if (empty($prototypes)) {
+                                break;
+                            }
+                            $removable_events = ($context->events)(Condition::makeFiltersFromPrototypes(...$prototypes));
                             foreach ($removable_events as $removable_event_id => $removable_event) {
                                 unset($context->events[$removable_event_id]);
                             }
--- tests/Unit/Relay/NIP9Test.php
+++ tests/Unit/Relay/NIP9Test.php
@@ -2,78 +2,89 @@
 
 use rikmeijer\Transpher\Relay;
 use rikmeijer\Transpher\Nostr\Key;
+use rikmeijer\Transpher\Nostr\Message;
 use function Pest\context;
 
-it('SHOULD NOT delete or stop publishing any referenced events that have an different pubkey as the deletion request.', function () {
-    $context = context();
+$references = [
+    'e' => fn(Message $message) => $message()[1]['id'],
+    'k' => fn(Message $message) => $message()[1]['kind']
+];
 
-    $sender_key = Key::generate();
-    $event = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 1, 'Hello World');
-    $event_id = $event()[1]['id'];
-    Relay::handle($event, $context);
-    expect($context->reply)->toHaveReceived(
-            ['OK']
-    );
+foreach ($references as $tag => $value_callback) {
+    it('SHOULD delete or stop publishing any referenced (' . $tag . ') events that have an identical pubkey as the deletion request.', function () use ($tag, $value_callback) {
+        $context = context();
 
-    Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
-    expect($context->reply)->toHaveReceived(
-            ['EVENT', $subscription_id, function (array $event) {
-                    expect($event['content'])->toBe('Hello World');
-                }],
-            ['EOSE', $subscription_id]
-    );
+        $sender_key = Key::generate();
+        $message = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 1, 'Hello World');
+        $referenced_value = $value_callback($message);
 
-    $delete_event = \rikmeijer\Transpher\Nostr\Message\Factory::event(Key::generate(), 5, 'sent by accident', ['e', $event_id]);
-    Relay::handle($delete_event, $context);
-    expect($context->reply)->toHaveReceived(
-            ['OK']
-    );
+        Relay::handle($message, $context);
+        expect($context->reply)->toHaveReceived(
+                ['OK']
+        );
 
-    expect(isset($context->events[$event_id]))->toBeTrue();
+        Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
+        expect($context->reply)->toHaveReceived(
+                ['EVENT', $subscription_id, function (array $event) {
+                        expect($event['content'])->toBe('Hello World');
+                    }],
+                ['EOSE', $subscription_id]
+        );
 
-    Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
-    expect($context->reply)->toHaveReceived(
-            ['EVENT', $subscription_id, function (array $event) {
-                    expect($event['content'])->toBe('Hello World');
-                }],
-            ['EOSE', $subscription_id]
-    );
-});
+        $delete_event = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 5, 'sent by accident', [$tag, $referenced_value]);
+        Relay::handle($delete_event, $context);
+        expect($context->reply)->toHaveReceived(
+                ['OK', $delete_event()[1]['id'], true]
+        );
 
+        expect(isset($context->events[$delete_event()[1]['id']]))->toBeTrue();
+        expect(isset($context->events[$message()[1]['id']]))->toBeFalse();
 
-it('SHOULD delete or stop publishing any referenced events that have an identical pubkey as the deletion request.', function () {
-    $context = context();
+        Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
+        expect($context->reply)->toHaveReceived(
+                ['EVENT', $subscription_id, function (array $event) {
+                        expect($event['content'])->toBe('sent by accident');
+                        expect($event['kind'])->toBe(5);
+                    }],
+                ['EOSE', $subscription_id]
+        );
+    });
 
-    $sender_key = Key::generate();
-    $event = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 1, 'Hello World');
-    $event_id = $event()[1]['id'];
-    Relay::handle($event, $context);
-    expect($context->reply)->toHaveReceived(
-            ['OK']
-    );
+    it('SHOULD NOT delete or stop publishing any referenced (' . $tag . ') events that have an different pubkey as the deletion request.', function () use ($tag, $value_callback) {
+        $context = context();
 
-    Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
-    expect($context->reply)->toHaveReceived(
-            ['EVENT', $subscription_id, function (array $event) {
-                    expect($event['content'])->toBe('Hello World');
-                }],
-            ['EOSE', $subscription_id]
-    );
+        $sender_key = Key::generate();
+        $message = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 1, 'Hello World');
+        $referenced_value = $value_callback($message);
 
-    $delete_event = \rikmeijer\Transpher\Nostr\Message\Factory::event($sender_key, 5, 'sent by accident', ['e', $event_id]);
-    Relay::handle($delete_event, $context);
-    expect($context->reply)->toHaveReceived(
-            ['OK']
-    );
+        Relay::handle($message, $context);
+        expect($context->reply)->toHaveReceived(
+                ['OK']
+        );
 
-    expect(isset($context->events[$event_id]))->ToBeFalse();
+        Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
+        expect($context->reply)->toHaveReceived(
+                ['EVENT', $subscription_id, function (array $event) {
+                        expect($event['content'])->toBe('Hello World');
+                    }],
+                ['EOSE', $subscription_id]
+        );
 
-    Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
-    expect($context->reply)->toHaveReceived(
-            ['EVENT', $subscription_id, function (array $event) {
-                    expect($event['content'])->toBe('sent by accident');
-                    expect($event['kind'])->toBe(5);
-                }],
-            ['EOSE', $subscription_id]
-    );
-});
+        $delete_event = \rikmeijer\Transpher\Nostr\Message\Factory::event(Key::generate(), 5, 'sent by accident', [$tag, $referenced_value]);
+        Relay::handle($delete_event, $context);
+        expect($context->reply)->toHaveReceived(
+                ['OK', $delete_event()[1]['id'], true]
+        );
+
+        expect(isset($context->events[$delete_event()[1]['id']]))->toBeTrue();
+        expect(isset($context->events[$message()[1]['id']]))->toBeTrue();
+
+        Relay::handle(json_encode(['REQ', $subscription_id = uniqid(), ['authors' => [$sender_key(Key::public())]]]), $context);
+        expect($context->reply)->toHaveReceived(
+                ['EVENT', $subscription_id, function (array $event) {
+                        expect($event['content'])->toBe('Hello World');
+                    }],
+                ['EOSE', $subscription_id]
+        );
+    });
+}

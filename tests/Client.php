f84a9644edc2a5c7049568a3dca4fb3331f49a4f
Rik Meijer <rik@nostriphant.dev>
2024-10-30 14:11:34 +0100

    fix recurring failing test in AgentTest
    (due to slow startup process)

--- tests/Client.php
+++ tests/Client.php
@@ -22,20 +22,20 @@ class Client extends \nostriphant\Transpher\Client {
     }
     
     public function expectNostrOK(string $eventId) {
-        $this->expected_messages[] = ['OK', function(callable $stop, array $message) use ($eventId) {
-            expect($message[0])->toBe($eventId);
+        $this->expected_messages[] = ['OK', function (array $message) use ($eventId) {
+                expect($message[0])->toBe($eventId);
             expect($message[1])->toBeTrue();
         }];
     }
     public function expectNostrEvent(string $subscriptionId, string $content) {
-        $this->expected_messages[] = ['EVENT', function(callable $stop, array $message) use ($subscriptionId, $content) {
-            expect($message[0])->toBe($subscriptionId);
+        $this->expected_messages[] = ['EVENT', function (array $message) use ($subscriptionId, $content) {
+                expect($message[0])->toBe($subscriptionId);
             expect($message[1]['content'])->toBe($content);
         }];
     }
     public function expectNostrPrivateDirectMessage(string $subscriptionId, Nostr\Key $recipient_key, string $message_content) {
-        $this->expected_messages[] = ['EVENT', function(callable $stop, array $message) use ($subscriptionId, $recipient_key, $message_content) {
-            expect($message[0])->toBe($subscriptionId);
+        $this->expected_messages[] = ['EVENT', function (array $message) use ($subscriptionId, $recipient_key, $message_content) {
+                expect($message[0])->toBe($subscriptionId);
             
             $gift = $message[1];
             expect($gift['kind'])->toBe(1059);
@@ -54,18 +54,18 @@ class Client extends \nostriphant\Transpher\Client {
         $this->expectNostrEose($subscriptionId);
     }
     public function expectNostrEose(string $subscriptionId) {
-        $this->expected_messages[] = ['EOSE', function(callable $stop, array $message) use ($subscriptionId) {
-            expect($message[0])->toBe($subscriptionId);
+        $this->expected_messages[] = ['EOSE', function (array $message) use ($subscriptionId) {
+                expect($message[0])->toBe($subscriptionId);
         }];
     }
     public function expectNostrNotice(string $expectedMessage) {
-        $this->expected_messages[] = ['NOTICE', function(callable $stop, array $message) use ($expectedMessage) {
-            expect($message[0])->toBe($expectedMessage);
+        $this->expected_messages[] = ['NOTICE', function (array $message) use ($expectedMessage) {
+                expect($message[0])->toBe($expectedMessage);
         }];
     }
     public function expectNostrClosed(string $subscriptionId, string $expected_message) {
-        $this->expected_messages[] = ['CLOSED', function(callable $stop, array $message) use ($subscriptionId, $expected_message) {
-            expect($message[0])->toBe($subscriptionId);
+        $this->expected_messages[] = ['CLOSED', function (array $message) use ($subscriptionId, $expected_message) {
+                expect($message[0])->toBe($subscriptionId);
             expect($message[1])->toBe($expected_message);
         }];
     }
@@ -83,11 +83,11 @@ class Client extends \nostriphant\Transpher\Client {
     }
 
     public function start(int $timeout = 5): void {
-        $this->onJson(function(callable $stop, array $message) {
+        $this->onJson(function (callable $stop, array $message) {
             $expected_message = array_shift($this->expected_messages);
             expect(array_shift($message))->toBe($expected_message[0], 'Message type checks out');
-            $expected_message[1]($stop, $message);
-            
+            $expected_message[1]($message);
+
             if (count($this->expected_messages) === 0) {
                 $stop();
             }
--- tests/Feature/AgentTest.php
+++ tests/Feature/AgentTest.php
@@ -7,7 +7,9 @@ use nostriphant\TranspherTests\Feature\Functions;
 
 describe('agent', function () : void {
     it('starts relay and sends private direct messsage to relay owner', function () : void {
-        $relay = Functions::bootRelay('127.0.0.1:8085', []);
+        $relay = Functions::bootRelay('127.0.0.1:8085', [
+                'RELAY_STORE' => ROOT_DIR . '/data/' . uniqid('relay_', true)
+        ]);
 
         $agent_key = \Pest\key_sender();
         $alice_key = \Pest\key_sender();
@@ -16,12 +18,11 @@ describe('agent', function () : void {
             'AGENT_NSEC' => $agent_key(Key::private(\nostriphant\Transpher\Nostr\Key\Format::BECH32)),
             'RELAY_URL' => 'ws://127.0.0.1:8085'
         ]);
-        
+        sleep(1); // hack to give agent some time to boot...
         $alice = \nostriphant\TranspherTests\Client::client(8085);
         $subscription = Factory::subscribe(
                 new Filter(tags: ['#p' => [$alice_key(Key::public())]])
         );
-        $alice->expectNostrEose($subscription()[1]);
         $alice->expectNostrPrivateDirectMessage($subscription()[1], $alice_key, 'Hello, I am your agent! The URL of your relay is ws://127.0.0.1:8085');
         $request = $subscription();
         $alice->json($request);

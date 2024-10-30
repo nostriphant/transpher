2a7a6eca63a968af966d2daaf991ed5eb161aca2
Rik Meijer <rik@nostriphant.dev>
2024-10-30 14:23:26 +0100

    refactor

--- tests/Unit/Nostr/NIP44/PaddingTest.php
+++ tests/Unit/Nostr/NIP44/PaddingTest.php
@@ -3,8 +3,6 @@
 use nostriphant\Transpher\Nostr\NIP44\Padding;
 use function Pest\vectors;
 
-it('calcs padded length correctly', function () {
-    foreach (vectors('nip44')->v2->valid->calc_padded_len as $vector) {
-        expect(Padding::calculateLength($vector[0]))->toBe($vector[1]);
-    }
-});
+it('calcs padded length correctly', function (int $unpadded_length, int $expected_length) {
+    expect(Padding::calculateLength($unpadded_length))->toBe($expected_length);
+})->with(vectors('nip44')->v2->valid->calc_padded_len);

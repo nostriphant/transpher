b62e6bfc195cd1ae0f52cbdd2ce13879fb7c24ad
Rik Meijer <rik@nostriphant.dev>
2024-10-30 12:45:16 +0100

    Update NIP17Test.php
    use new way of interfacing with Relay

--- tests/Unit/Client/NIP17Test.php
+++ tests/Unit/Client/NIP17Test.php
@@ -1,30 +1,41 @@
 <?php
 
 use nostriphant\Transpher\Nostr\Key;
-use nostriphant\Transpher\Nostr\Message\Factory;
-use nostriphant\Transpher\Nostr\Subscription\Filter;
-use nostriphant\TranspherTests\Unit\Client;
+use nostriphant\Transpher\Relay;
+use nostriphant\Transpher\Nostr\Event\Gift;
+use nostriphant\Transpher\Nostr\Event\Seal;
+use function \Pest\context;
 
 it('relays private direct messsage from alice to bob', function (): void {
-    $transpher_store = ROOT_DIR . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . uniqid();
-    mkdir($transpher_store);
 
-    $alice = Client::persistent_client($transpher_store);
+    $context = context();
+
     $alice_key = \Pest\key_sender();
 
     $bob_key = \Pest\key_recipient();
+    $event = \nostriphant\Transpher\Nostr\Message\Factory::privateDirect($alice_key, $bob_key(Key::public(Key\Format::HEXIDECIMAL)), 'Hello!!');
 
-    $alice->privateDirectMessage($alice_key, $bob_key(Key::public(Key\Format::BECH32)), 'Hello!!');
-
-    $subscription = Factory::subscribe(
-            new Filter(tags: ['#p' => [$bob_key(Key::public())]])
+    Relay::handle($event, $context);
+    expect($context->reply)->toHaveReceived(
+            ['OK']
     );
 
-    $bob = Client::persistent_client($transpher_store);
-    $bob->expectNostrPrivateDirectMessage($subscription()[1], $bob_key, 'Hello!!');
-    $request = $subscription();
-    expect($request[2])->toBeArray();
-    expect($request[2]['#p'])->toContain($bob_key(Key::public()));
-    $bob->json($request);
-    $bob->start();
+    Relay::handle(json_encode(['REQ', $subscriptionId = uniqid(), ['#p' => [$bob_key(Key::public())]]]), $context);
+    expect($context->reply)->toHaveReceived(
+            ['EVENT', $subscriptionId, function (array $gift) use ($bob_key) {
+                    expect($gift['kind'])->toBe(1059);
+
+                    $seal = Gift::unwrap($bob_key, $gift['pubkey'], $gift['content']);
+                    expect($seal['kind'])->toBe(13);
+                    expect($seal['pubkey'])->toBeString();
+                    expect($seal['content'])->toBeString();
+
+                    $private_message = Seal::open($bob_key, $seal['pubkey'], $seal['content']);
+                    expect($private_message)->toBeArray();
+                    expect($private_message)->toHaveKey('id');
+                    expect($private_message)->toHaveKey('content');
+                    expect($private_message['content'])->toBe('Hello!!');
+                }],
+            ['EOSE', $subscriptionId]
+    );
 });

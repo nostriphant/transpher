9791856dd25b2259cea7f03e7ebe17c1a612d5a2
Rik Meijer <rik@nostriphant.dev>
2024-11-05 15:05:35 +0100

    use limits actually in Relay

--- src/Relay/Incoming/Req.php
+++ src/Relay/Incoming/Req.php
@@ -11,7 +11,8 @@ readonly class Req implements Type {
 
     public function __construct(
             private \nostriphant\Transpher\Relay\Store $events,
-            private \nostriphant\Transpher\Relay\Subscriptions $subscriptions
+            private \nostriphant\Transpher\Relay\Subscriptions $subscriptions,
+            private \nostriphant\Transpher\Relay\Limits $limits
     ) {
         
     }
@@ -21,15 +22,24 @@ readonly class Req implements Type {
         if (count($payload) < 2) {
             yield Factory::notice('Invalid message');
         } else {
-            $filter_prototypes = array_filter(array_slice($payload, 1));
+            $constraint = ($this->limits)($this->subscriptions);
+            switch ($constraint->result) {
+                case Constraint\Result::REJECTED:
+                    yield Factory::closed($payload[0], $constraint->reason);
+                    break;
 
-            if (count($filter_prototypes) === 0) {
-                yield Factory::closed($payload[0], 'Subscription filters are empty');
-            } else {
-                $filters = Condition::makeFiltersFromPrototypes(...$filter_prototypes);
-                ($this->subscriptions)($payload[0], $filters);
-                yield from map(($this->events)($filters), partial_left([Factory::class, 'requestedEvent'], $payload[0]));
-                yield Factory::eose($payload[0]);
+                case Constraint\Result::ACCEPTED:
+                    $filter_prototypes = array_filter(array_slice($payload, 1));
+
+                    if (count($filter_prototypes) === 0) {
+                        yield Factory::closed($payload[0], 'Subscription filters are empty');
+                    } else {
+                        $filters = Condition::makeFiltersFromPrototypes(...$filter_prototypes);
+                        ($this->subscriptions)($payload[0], $filters);
+                        yield from map(($this->events)($filters), partial_left([Factory::class, 'requestedEvent'], $payload[0]));
+                        yield Factory::eose($payload[0]);
+                    }
+                    break;
             }
         }
     }
--- tests/Unit/Relay/DefaultLimitsTest.php
+++ tests/Unit/Relay/DefaultLimitsTest.php
@@ -13,7 +13,7 @@ it('SHOULD ignore created_at limits for regular events', function () {
     expect($recipient_future)->toHaveReceived(
             ['OK', $message_future()[1]['id'], true]
     );
-})->with();
+});
 
 it('SHOULD send the client an OK result saying the event was not stored for the created_at timestamp not being within the permitted limits.', function (int $kind) {
     $recipient_past = \Pest\handle($message_past = Factory::eventAt(\Pest\key_sender(), $kind, 'Hello World', time() - (60 * 60 * 24) - 5));
@@ -30,3 +30,17 @@ it('SHOULD send the client an OK result saying the event was not stored for the 
     'ephemeral' => 20000,
     'addressable' => 30000
 ]);
+
+
+it('SHOULD deny more subscriptions than 10 per client, by default', function () {
+    $subscriptions = \Pest\subscriptions();
+
+    for ($s = 1; $s < 11; $s++) {
+        $recipient_past = \Pest\handle($message_past = Factory::req('subscription' . $s . '-id', ['ids' => ['sdsd']]), subscriptions: $subscriptions);
+        expect($recipient_past)->toHaveReceived(['EOSE', 'subscription' . $s . '-id']);
+    }
+
+
+    $recipient_past = \Pest\handle($message_past = Factory::req('subscription11-id', ['ids' => ['sdsd']]), subscriptions: $subscriptions);
+    expect($recipient_past)->toHaveReceived(['CLOSED', 'subscription11-id', 'max number of subscriptions per client (10) reached']);
+});

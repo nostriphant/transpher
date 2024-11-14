3f62859a86a63793762aadf0754072bf2241e826
Rik Meijer <rik@nostriphant.dev>
2024-11-14 21:21:09 +0100

    make NIP92 support explicit

--- src/Relay/InformationDocument.php
+++ src/Relay/InformationDocument.php
@@ -9,7 +9,7 @@ class InformationDocument {
             "description" => $description,
             "pubkey" => \nostriphant\Transpher\Nostr\Bech32::fromNpub($npub),
             "contact" => $contact,
-            "supported_nips" => [1, 2, 9, 11, 12, 13, 16, 20, 22, 33, 45, 94],
+            "supported_nips" => [1, 2, 9, 11, 12, 13, 16, 20, 22, 33, 45, 92, 94],
             "software" => 'Transpher',
             "version" => 'dev'
         ];

6219de62c8425d2026ae46cd8303e404363fca7a
Rik Meijer <rik@nostriphant.dev>
2024-11-21 10:11:03 +0100

    refactor

--- src/Nostr/Subscription.php
+++ src/Nostr/Subscription.php
@@ -3,22 +3,21 @@ namespace nostriphant\Transpher\Nostr;
 
 use function Functional\some,
              Functional\map,
-             Functional\true,
-             Functional\partial_left;
+             Functional\true;
 use nostriphant\Transpher\Nostr\Subscription\Filter;
 use nostriphant\NIP01\Event;
 
 readonly class Subscription {
 
-    private function __construct(private \Closure $filters) {
+    private function __construct(private array $filters) {
         
     }
     
     public function __invoke(Event $event) : bool {
-        return some(($this->filters)(fn(array $possible_filter) => true(map($possible_filter, fn(callable $subscription_filter) => $subscription_filter($event)))));
+        return some(array_map(fn(array $possible_filter) => true(array_map(fn(callable $subscription_filter) => $subscription_filter($event), $possible_filter)), $this->filters));
     }
 
     static function make(callable $to, array ...$filter_prototypes): self {
-        return new self(partial_left('\Functional\map', array_map(fn(array $filter_prototype) => $to(Filter::fromPrototype($filter_prototype)), $filter_prototypes)));
+        return new self(array_map(fn(array $filter_prototype) => $to(Filter::fromPrototype($filter_prototype)), $filter_prototypes));
     }
 }

e5b0aeba3e8284db07c458f6d44f0693c25be022
Rik Meijer <rik@nostriphant.dev>
2024-12-02 10:03:15 +0100

    cleanup files on construction

--- src/Files.php
+++ src/Files.php
@@ -6,6 +6,42 @@ readonly class Files {
 
     public function __construct(private string $path, private Relay\Store $store) {
         is_dir($path) || mkdir($path);
+
+        foreach (glob($path . DIRECTORY_SEPARATOR . '*') as $file) {
+            if (is_file($file) === false) {
+                continue;
+            }
+
+            $events_path = self::makeEventsPath($file);
+            if (is_dir($events_path) === false) {
+                unlink($file);
+                continue;
+            }
+            $event_files = glob($events_path . DIRECTORY_SEPARATOR . '*');
+            if (count($event_files) === 0) {
+                rmdir($events_path);
+                unlink($file);
+                continue;
+            }
+
+            foreach ($event_files as $event_file) {
+                $event_id = basename($event_file);
+                if (isset($this->store[$event_id]) === false) {
+                    unlink($event_file);
+                }
+            }
+
+            if (count(glob($events_path . DIRECTORY_SEPARATOR . '*')) === 0) {
+                rmdir($events_path);
+                unlink($file);
+                continue;
+            }
+        }
+    }
+
+
+    static function makeEventsPath(string $file_path) {
+        return $file_path . '.events';
     }
 
     public function __invoke(string $hash): object {
@@ -33,7 +69,7 @@ readonly class Files {
                 fclose($remote_handle);
                 fclose($local_handle);
 
-                $events_path = $this->path . '.events';
+                $events_path = Files::makeEventsPath($this->path);
                 is_dir($events_path) || mkdir($events_path);
                 touch($events_path . '/' . $event_id);
                 return null;
--- tests/Unit/FilesTest.php
+++ tests/Unit/FilesTest.php
@@ -35,3 +35,55 @@ it('ignores file, when event is NOT in store', function () {
     expect(ROOT_DIR . '/data/files/' . $hash)->not()->toBeFile();
     expect(ROOT_DIR . '/data/files/' . $hash . '.events/' . $event_id)->not()->toBeFile();
 });
+
+
+it('removes files, when no events directory', function () {
+    $store = new nostriphant\Transpher\Stores\Memory([]);
+
+
+    $hash = uniqid();
+
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events')->not()->toBeDirectory();
+
+    file_put_contents(ROOT_DIR . '/data/files/' . $hash, uniqid());
+    mkdir(ROOT_DIR . '/data/files/' . $hash . '.events');
+    expect(ROOT_DIR . '/data/files/' . $hash)->toBeFile();
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events')->toBeDirectory();
+
+    $files = new Files(ROOT_DIR . '/data/files/', $store);
+
+    expect(ROOT_DIR . '/data/files/' . $hash)->not()->toBeFile();
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events')->not()->toBeDirectory();
+});
+
+it('removes files, when no events in events directory exist', function () {
+    $store = new nostriphant\Transpher\Stores\Memory([]);
+
+    $hash = uniqid();
+    file_put_contents(ROOT_DIR . '/data/files/' . $hash, uniqid());
+    expect(ROOT_DIR . '/data/files/' . $hash)->toBeFile();
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events')->not()->toBeDirectory();
+
+    $files = new Files(ROOT_DIR . '/data/files/', $store);
+
+    expect(ROOT_DIR . '/data/files/' . $hash)->not()->toBeFile();
+});
+
+it('removes files, when event is NOT in store', function () {
+    $event_id = uniqid();
+
+    $store = new nostriphant\Transpher\Stores\Memory([]);
+
+    $hash = uniqid();
+    file_put_contents(ROOT_DIR . '/data/files/' . $hash, uniqid());
+    mkdir(ROOT_DIR . '/data/files/' . $hash . '.events');
+    touch(ROOT_DIR . '/data/files/' . $hash . '.events/' . $event_id);
+    expect(ROOT_DIR . '/data/files/' . $hash)->toBeFile();
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events')->toBeDirectory();
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events/' . $event_id)->toBeFile();
+
+    $files = new Files(ROOT_DIR . '/data/files/', $store);
+
+    expect(ROOT_DIR . '/data/files/' . $hash . '.events/' . $event_id)->not()->toBeFile();
+    expect(ROOT_DIR . '/data/files/' . $hash)->not()->toBeFile();
+});

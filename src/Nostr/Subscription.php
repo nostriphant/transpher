89980caf02a20d2e472e842ac7cbd71a7da3177c
Rik Meijer <rik@nostriphant.dev>
2024-12-13 12:15:08 +0100

    do not store disabled as property

--- src/Nostr/Subscription.php
+++ src/Nostr/Subscription.php
@@ -7,18 +7,22 @@ use nostriphant\NIP01\Event;
 
 readonly class Subscription {
 
-    public function __construct(public bool $disabled, private \Closure $test) {
+    public function __construct(private \Closure $test) {
         
     }
     
     public function __invoke() {
-        return $this->disabled ? null : call_user_func_array($this->test, func_get_args());
+        return call_user_func_array($this->test, func_get_args());
     }
 
     static function make(array $filter_prototypes, string $mapperClass): self {
-        $to = new Conditions($mapperClass);
         $disabled = empty($filter_prototypes) || array_reduce($filter_prototypes, fn(bool $disabled, array $filter_prototype) => empty($filter_prototype), true);
+        if ($disabled) {
+            return new self(fn() => null);
+        }
+
+        $to = new Conditions($mapperClass);
         $filters = $to->filters($filter_prototypes);
-        return new self($disabled, $mapperClass::wrapFilters($filters));
+        return new self($mapperClass::wrapFilters($filters));
     }
 }

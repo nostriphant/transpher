3cb590b245c7841c37cf64f025aee460fbdd5a24
Rik Meijer <rik@nostriphant.dev>
2024-12-13 20:53:14 +0100

    refactor

--- src/Nostr/Subscription.php
+++ src/Nostr/Subscription.php
@@ -6,13 +6,10 @@ readonly class Subscription {
     private \Closure $test;
 
     public function __construct(private array $filter_prototypes, string $mapperClass) {
-        $this->test = $mapperClass::makeConditions($filter_prototypes);
+        $this->test = self::disabled($this->filter_prototypes) ? fn() => null : $mapperClass::makeConditions($filter_prototypes);
     }
     
     public function __invoke() {
-        if (self::disabled($this->filter_prototypes)) {
-            return;
-        }
         return call_user_func_array($this->test, func_get_args());
     }
 

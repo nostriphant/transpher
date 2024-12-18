93e1d3874d4b7a7e1ef7d59c2c28500f8a80804f
Rik Meijer <rik@nostriphant.dev>
2024-12-18 16:14:20 +0100

    refactor

--- src/Relay/Conditions.php
+++ src/Relay/Conditions.php
@@ -9,13 +9,12 @@ readonly class Conditions {
     }
 
     public function __invoke(ConditionFactory $conditionFactory): array {
-        return array_map(
-                fn(array $filter_prototype) => array_map(
-                                $conditionFactory,
-                                array_keys($filter_prototype),
-                                $filter_prototype
-                        ),
-                        $this->filter_prototypes
-                );
+        return array_map(fn(array $filter_prototype) => array_map(
+                        $conditionFactory,
+                        array_keys($filter_prototype),
+                        $filter_prototype
+                ),
+                $this->filter_prototypes
+        );
     }
 }

eecaaf0acc1d40c7de92429a5fed33bb0c2cbc88
Rik Meijer <rik@nostriphant.dev>
2025-02-13 12:53:57 +0100

    use env var to toggle authentication

--- relay.php
+++ relay.php
@@ -46,7 +46,7 @@ $store = new nostriphant\Stores\Store($events, [
     ['#p' => $whitelisted_pubkeys]
         ]);
 
-$relay = new \nostriphant\Transpher\Amp\Relay($store, $files_path);
+$relay = new \nostriphant\Transpher\Amp\Relay($store, $files_path, $_SERVER['RELAY_ENABLE_AUTHENTICATION'] === '1');
 
 $args = explode(":", $_SERVER['argv'][1]);
 $args[] = $_SERVER['RELAY_MAX_CONNECTIONS_PER_IP'] ?? 1000;

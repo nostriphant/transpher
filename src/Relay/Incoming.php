a9b49418d42d7817889ed8a44868e3809bf7ca60
Rik Meijer <rik@nostriphant.dev>
2025-02-20 10:53:15 +0100

    use Alternate instead of match

--- src/Relay/Incoming.php
+++ src/Relay/Incoming.php
@@ -7,13 +7,13 @@ use nostriphant\NIP01\Message;
 readonly class Incoming {
 
     public function __invoke(\nostriphant\Transpher\Relay\Incoming\Context $context, Message $message): \Traversable {
-        yield from (match (strtoupper($message->type)) {
-                    'AUTH' => new Incoming\Auth(Incoming\Auth\Limits::fromEnv()),
-                    'EVENT' => new Incoming\Event(new Incoming\Event\Accepted($context), Incoming\Event\Limits::fromEnv()),
-                    'CLOSE' => new Incoming\Close($context),
-                    'REQ' => new Incoming\Req(new Incoming\Req\Accepted($context, Incoming\Req\Accepted\Limits::fromEnv()), Incoming\Req\Limits::fromEnv()),
-                    'COUNT' => new Incoming\Count($context, Incoming\Count\Limits::fromEnv()),
-                    default => new Incoming\Unknown($message->type)
-                })($message->payload);
+        yield from \nostriphant\FunctionalAlternate\Alternate::{$message->type}($message->payload)(
+                        AUTH: new Incoming\Auth(Incoming\Auth\Limits::fromEnv()),
+                        EVENT: new Incoming\Event(new Incoming\Event\Accepted($context), Incoming\Event\Limits::fromEnv()),
+                        CLOSE: new Incoming\Close($context),
+                        REQ: new Incoming\Req(new Incoming\Req\Accepted($context, Incoming\Req\Accepted\Limits::fromEnv()), Incoming\Req\Limits::fromEnv()),
+                        COUNT: new Incoming\Count($context, Incoming\Count\Limits::fromEnv()),
+                        default: new Incoming\Unknown($message->type)
+                );
     }
 }

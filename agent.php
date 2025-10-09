38f786f285f626d3af5b8b9275311e0bce10150a
Rik Meijer <github@rikmeijer.nl>
2025-10-09 16:32:21 +0200

    fix breaking changes

--- agent.php
+++ agent.php
@@ -1,8 +1,12 @@
 <?php
 
+use nostriphant\NIP01\Key;
+use nostriphant\NIP19\Bech32;
+use nostriphant\Transpher\Agent;
+
 $log = (require_once __DIR__ . '/bootstrap.php')('agent', 'INFO', $_SERVER['AGENT_LOG_LEVEL'] ?? 'INFO');
 
-$agent = new nostriphant\Transpher\Agent(
+$agent = new Agent(
         Key::fromHex((new Bech32($_SERVER['AGENT_NSEC']))()),
         new Bech32($_SERVER['RELAY_OWNER_NPUB'])
 );

1ac108559d5876a085370bee6bde319a10d89b83
Rik Meijer <github@rikmeijer.nl>
2025-10-15 14:06:55 +0200

    instantiate websocket client when invoking client

--- src/Nostr/Client.php
+++ src/Nostr/Client.php
@@ -3,15 +3,13 @@
 namespace nostriphant\Transpher\Nostr;
 
 readonly class Client {
-
-    private \nostriphant\Transpher\Amp\Client $client;
     
-    public function __construct(string $relay_url, private \Closure $response_callback) {
-        $this->client = new \nostriphant\Transpher\Amp\Client(0, $relay_url);
+    public function __construct(private string $relay_url, private \Closure $response_callback) {
     }
     
     public function __invoke(callable $bootstrap_callback, callable $shutdown_callback): void {
-        $bootstrap_callback($this->client->start($this->response_callback));
-        $this->client->listen($shutdown_callback);
+        $client = new \nostriphant\Transpher\Amp\Client(0, $this->relay_url);
+        $bootstrap_callback($client->start($this->response_callback));
+        $client->listen($shutdown_callback);
     }
 }

e9d4806becf4d011f8d96e77c1a72244f347268a
Rik Meijer <github@rikmeijer.nl>
2025-10-15 11:30:56 +0200

    restructering Agent
    In time we want to extract the nostr-client specifics

--- agent.php
+++ agent.php
@@ -3,19 +3,24 @@
 use nostriphant\NIP01\Key;
 use nostriphant\NIP19\Bech32;
 use nostriphant\Transpher\Agent;
+use nostriphant\NIP17\PrivateDirect;
+use nostriphant\NIP01\Message;
 
 $log = (require_once __DIR__ . '/bootstrap.php')('agent', 'INFO', $_SERVER['AGENT_LOG_LEVEL'] ?? 'INFO');
 
-$agent = new Agent(
-        Key::fromHex((new Bech32($_SERVER['AGENT_NSEC']))()),
-        new Bech32($_SERVER['RELAY_OWNER_NPUB']),
-        $_SERVER['RELAY_URL']
-);
 
-$log->info('Client connecting to ' . $_SERVER['RELAY_URL'], $_SERVER['RELAY_URL']);
+$log->info('Client connecting to ' . $_SERVER['RELAY_URL']);
+$agent = new Agent($_SERVER['RELAY_URL']);
+
+$await = $agent(function(callable $send) use ($log) {
+    $key = Key::fromHex((new Bech32($_SERVER['AGENT_NSEC']))());
+    $relay_owner_npub = new Bech32($_SERVER['RELAY_OWNER_NPUB']);
+    
+    $log->info('Running agent with public key ' . $key(Key::public()));
+    $log->info('Sending Private Direct Message event');
+    $gift = PrivateDirect::make($key, $relay_owner_npub(), 'Hello, I am your agent! The URL of your relay is ' . $_SERVER['RELAY_URL']);
+    $send(Message::event($gift));
+});
+
 $log->info('Listening to relay...');
-$await = $agent();
-$log->info('Running agent with public key ' . Bech32::npub(($_SERVER['AGENT_NSEC'])(Key::public())));
-$log->info('Sending Private Direct Message event');
-
 $await(fn(int $signal) => $log->info(sprintf("Received signal %d, stopping agent", $signal)));
--- src/Agent.php
+++ src/Agent.php
@@ -5,27 +5,20 @@ namespace nostriphant\Transpher;
 use nostriphant\Transpher\Amp\Client;
 use nostriphant\Transpher\Amp\AwaitSignal;
 
-use nostriphant\NIP01\Key;
-use nostriphant\NIP19\Bech32;
 use nostriphant\NIP01\Message;
-use nostriphant\NIP17\PrivateDirect;
 
 readonly class Agent {
 
     private Client $client;
     
-    public function __construct(#[\SensitiveParameter] private Key $key, private Bech32 $relay_owner_npub, private string $relay_url) {
+    public function __construct(private string $relay_url) {
         $this->client = new Client(0, $relay_url);
     }
     
-    public function __invoke(): AwaitSignal {
-        $send = $this->client->start(function (Message $message) {
+    public function __invoke(callable $logic): AwaitSignal {
+        $logic($this->client->start(function (Message $message) {
             
-        });
-
-        $gift = PrivateDirect::make($this->key, call_user_func($this->relay_owner_npub), 'Hello, I am your agent! The URL of your relay is ' . $this->relay_url);
-        $send(Message::event($gift));
-
+        }));
         return $this->client->listen();
     }
 }
